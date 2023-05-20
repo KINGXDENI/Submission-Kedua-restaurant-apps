3415d004d850895f45753b1749608242e2ffc312
KINGBOTs1 <ds767365@gmail.com>
2023-05-20 17:34:53 +0800

    s

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -11,6 +11,7 @@ class App {
     this._button = button;
     this._drawer = drawer;
     this._content = content;
+    this._previousUrl = null;
 
     this._initialAppShell();
   }
@@ -30,22 +31,33 @@ class App {
     const page = routes[url];
 
     if (page && page.render) {
-      this._showLoadingIndicator();
-
-      try {
-        await this._wait(500);
-        const renderedContent = await page.render();
-        this._content.innerHTML = renderedContent;
+      if (this._isSamePage(url)) {
+        this._content.innerHTML = await page.render();
         await page.afterRender();
-      } catch (error) {
-        this._showErrorPage();
-        console.error('Failed to render page:', error);
-      }
+      } else {
+        this._showLoadingIndicator();
 
-      this._hideLoadingIndicator();
+        try {
+          await this._wait(500);
+          const renderedContent = await page.render();
+          this._content.innerHTML = renderedContent;
+          await page.afterRender();
+        } catch (error) {
+          this._showErrorPage();
+          console.error('Failed to render page:', error);
+        }
+
+        this._hideLoadingIndicator();
+      }
     } else {
       this._showErrorPage();
     }
+
+    this._previousUrl = url;
+  }
+
+  _isSamePage(url) {
+    return this._previousUrl === url;
   }
 
   // eslint-disable-next-line class-methods-use-this
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
-    port: 9124,
+    port: 9144,
     client: {
       overlay: {
         errors: true,
